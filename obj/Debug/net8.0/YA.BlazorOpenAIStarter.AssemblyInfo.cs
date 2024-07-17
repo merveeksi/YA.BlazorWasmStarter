a21716059c48324f076f8670fbb691dcc91f7483
MERVE EKŞİ <merveeksii61@gmail.com>
2024-07-17 12:06:11 +0300

    snh

--- obj/Debug/net8.0/YA.BlazorOpenAIStarter.AssemblyInfo.cs
+++ obj/Debug/net8.0/YA.BlazorOpenAIStarter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YA.BlazorOpenAIStarter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c79eb7aefc6deb2a166243d2e78611ed602f262")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bddf9ce686743eb648c4dadf367ff28097d026e")]
 [assembly: System.Reflection.AssemblyProductAttribute("YA.BlazorOpenAIStarter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YA.BlazorOpenAIStarter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
